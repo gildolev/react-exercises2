ae400bc7d0f72b32e1ed674534726efa99765bf8
gildolev <info@sabonsabon.com>
2021-08-16 17:01:54 +0300

    change wether to reduce

--- src/Weather/Weather.js
+++ src/Weather/Weather.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import './Weather.css';
 
 function Weather() {
-	const days=['Mon','Tue','Wed','Thu','Fri'];
+	const days = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri'];
 	const [weekForcast, setWeekForcast] = useState([]);
 	const [minDay, setMinDay] = useState(null);
 	const [maxDay, setMaxDay] = useState(null);
@@ -12,24 +12,51 @@ function Weather() {
 			.then(results => results.json())
 			.then(weekForcast => setWeekForcast(weekForcast))
 	}, [])
+	// useEffect(() => {
+	// 	if (weekForcast.length > 0) {
+	// 		let minDay = 0;
+	// 		let maxDay = 0;
+	// 		setMaxDay(days[0])
+	// 		setMinDay(days[0])
+	// 		for (let currentDay = 0; currentDay < weekForcast.length; currentDay++) {
+	// 			if (weekForcast[currentDay].temperature > weekForcast[maxDay].temperature) {
+	// 				maxDay = currentDay;
+	// 				setMaxDay(days[weekForcast[currentDay].day - 2]);
+	// 			}
+	// 			if (weekForcast[currentDay].temperature < weekForcast[minDay].temperature) {
+	// 				minDay = currentDay;
+	// 				setMinDay(days[weekForcast[currentDay].day - 2]);
+	// 			}
+	// 		}
+	// 	}
+	// }, [weekForcast])
 	useEffect(() => {
-		if(weekForcast.length>0){
-		let minDay = 0;
-		let maxDay = 0;
-		setMaxDay(days[0])
-		setMinDay(days[0])
-		for (let currentDay = 0; currentDay < weekForcast.length; currentDay++) {
-			if (weekForcast[currentDay].temperature > weekForcast[maxDay].temperature) {
-				maxDay=currentDay;
-				setMaxDay(days[weekForcast[currentDay].day-2]);
-			}
-			if (weekForcast[currentDay].temperature < weekForcast[minDay].temperature) {
-				minDay=currentDay;
-				setMinDay(days[weekForcast[currentDay].day-2]);
-			}
-		}}
-	}, [weekForcast])
+		if (weekForcast.length > 0) {
+		let min=weekForcast[0].day;
+	    let max=weekForcast[0].day;
+		
+			const initialValue = weekForcast[0].temperature;
+			 weekForcast.reduce((accumulator, currentValue) => {
+				if(currentValue.temperature<accumulator){
+					min=currentValue.day;
+				}
+			return	Math.min(accumulator, currentValue.temperature)
+			}, initialValue);
 
+			 weekForcast.reduce((accumulator, currentValue) => {
+				if(currentValue.temperature>accumulator){
+					max=currentValue.day;
+				}
+			return	Math.max(accumulator, currentValue.temperature)
+			}, initialValue);
+
+
+ 
+			setMinDay(days[min - 2]);
+			setMaxDay(days[max - 2]);
+		}
+		
+	}, [weekForcast])
 	return (
 		<div className="Weather">
 			<p>
